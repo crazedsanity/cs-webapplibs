4ea95eabc7b8af12875bf8d7fa81adeb849da014
crazedsanity <crazedsanity@59f08903-b221-44ba-a638-31359e0048a4>
2009-08-19 16:10:34 +0000

    Copy tests folder for cs_versionAbstract.

---
+++ trunk/0.3/tests/example_test.php
@@ -0,0 +1,19 @@
+<?php
+/*
+ * Created on Jan 25, 2009
+ * 
+ * FILE INFORMATION:
+ * 
+ * $HeadURL$
+ * $Id$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $LastChangedRevision$
+ */
+
+
+require_once(dirname(__FILE__) .'/testOfCSVersionParse.php');
+
+$test = &new TestOfA2P();
+$test->run(new HtmlReporter())
+?>
