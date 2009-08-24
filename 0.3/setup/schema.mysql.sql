42ac5333acd33ca0927c81ab4b4691c479512285
Dan Falconer <github@crazedsanity.com>
2009-08-24 16:25:59 +0000

    Added cswal_attribute_table definition.

--- 0.3/setup/schema.mysql.sql
+++ 0.3/setup/schema.mysql.sql
@@ -1,23 +1,25 @@
---
--- SVN INFORMATION:::
--- ---------------
---	SVN Signature::::::: $Id$
---	Last Author::::::::: $Author$
---	Current Revision:::: $Revision$
---	Repository Location: $HeadURL$
---	Last Updated:::::::: $Date$
---
-
 -- phpMyAdmin SQL Dump
 -- version 2.10.3
 -- http://www.phpmyadmin.net
 -- 
--- Generation Time: Aug 24, 2009 at 10:55 AM
+-- Generation Time: Aug 24, 2009 at 11:31 AM
 -- Server version: 5.0.22
 -- PHP Version: 5.1.6
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `cswal_attribute_table`
+-- 
+
+CREATE TABLE `cswal_attribute_table` (
+  `attribute_id` int(11) NOT NULL auto_increment,
+  `attribute_name` text NOT NULL,
+  PRIMARY KEY  (`attribute_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -37,7 +39,7 @@ CREATE TABLE `cswal_auth_token_table` (
   `expiration` timestamp NOT NULL default '0000-00-00 00:00:00',
   PRIMARY KEY  (`auth_token_id`),
   UNIQUE KEY `auth_token_id` (`auth_token_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -49,7 +51,7 @@ CREATE TABLE `cswal_category_table` (
   `category_id` int(11) NOT NULL auto_increment,
   `category_name` text NOT NULL,
   PRIMARY KEY  (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -61,7 +63,7 @@ CREATE TABLE `cswal_class_table` (
   `class_id` int(11) NOT NULL auto_increment,
   `class_name` text NOT NULL,
   PRIMARY KEY  (`class_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -77,7 +79,7 @@ CREATE TABLE `cswal_event_table` (
   PRIMARY KEY  (`event_id`),
   KEY `cswal_event_table_class_id_fkey` (`class_id`),
   KEY `cswal_event_table_category_id_fkey` (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -93,7 +95,7 @@ CREATE TABLE `cswal_log_attribute_table` (
   PRIMARY KEY  (`log_attribute_id`),
   KEY `cswal_log_attribute_table_log_id_fkey` (`log_id`),
   KEY `cswal_log_attribute_table_attribute_id_fkey` (`attribute_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -110,7 +112,7 @@ CREATE TABLE `cswal_log_table` (
   `details` text NOT NULL,
   PRIMARY KEY  (`log_id`),
   KEY `cswal_log_table_event_id_fkey` (`event_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -126,7 +128,7 @@ CREATE TABLE `cswal_session_store_table` (
   `last_updated` datetime NOT NULL,
   `session_data` longtext NOT NULL,
   PRIMARY KEY  (`session_store_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -140,7 +142,7 @@ CREATE TABLE `cswal_version_table` (
   `version_string` varchar(50) NOT NULL,
   PRIMARY KEY  (`version_id`),
   UNIQUE KEY `project_name` (`project_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- 
 -- Constraints for dumped tables
