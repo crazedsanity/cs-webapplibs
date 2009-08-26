ff3b38b2f873b33a25e45b85b43a406c44963656
crazedsanity <crazedsanity@59f08903-b221-44ba-a638-31359e0048a4>
2009-08-26 17:50:33 +0000

    Updated schema with relationship comments & a required foreign key.

--- trunk/0.3/setup/schema.mysql.sql
+++ trunk/0.3/setup/schema.mysql.sql
@@ -2,7 +2,7 @@
 -- version 2.10.3
 -- http://www.phpmyadmin.net
 -- 
--- Generation Time: Aug 24, 2009 at 11:31 AM
+-- Generation Time: Aug 26, 2009 at 12:54 PM
 -- Server version: 5.0.22
 -- PHP Version: 5.1.6
 
@@ -79,7 +79,15 @@ CREATE TABLE `cswal_event_table` (
   PRIMARY KEY  (`event_id`),
   KEY `cswal_event_table_class_id_fkey` (`class_id`),
   KEY `cswal_event_table_category_id_fkey` (`category_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- 
+-- RELATIONS FOR TABLE `cswal_event_table`:
+--   `class_id`
+--       `cswal_class_table` -> `class_id`
+--   `category_id`
+--       `cswal_category_table` -> `category_id`
+-- 
 
 -- --------------------------------------------------------
 
@@ -95,7 +103,15 @@ CREATE TABLE `cswal_log_attribute_table` (
   PRIMARY KEY  (`log_attribute_id`),
   KEY `cswal_log_attribute_table_log_id_fkey` (`log_id`),
   KEY `cswal_log_attribute_table_attribute_id_fkey` (`attribute_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- 
+-- RELATIONS FOR TABLE `cswal_log_attribute_table`:
+--   `attribute_id`
+--       `cswal_attribute_table` -> `attribute_id`
+--   `log_id`
+--       `cswal_log_table` -> `log_id`
+-- 
 
 -- --------------------------------------------------------
 
@@ -112,7 +128,13 @@ CREATE TABLE `cswal_log_table` (
   `details` text NOT NULL,
   PRIMARY KEY  (`log_id`),
   KEY `cswal_log_table_event_id_fkey` (`event_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- 
+-- RELATIONS FOR TABLE `cswal_log_table`:
+--   `event_id`
+--       `cswal_event_table` -> `event_id`
+-- 
 
 -- --------------------------------------------------------
 
@@ -159,6 +181,7 @@ ALTER TABLE `cswal_event_table`
 -- Constraints for table `cswal_log_attribute_table`
 -- 
 ALTER TABLE `cswal_log_attribute_table`
+  ADD CONSTRAINT `cswal_log_attribute_table_attribute_id_fkey` FOREIGN KEY (`attribute_id`) REFERENCES `cswal_attribute_table` (`attribute_id`),
   ADD CONSTRAINT `cswal_log_attribute_table_log_id_fkey` FOREIGN KEY (`log_id`) REFERENCES `cswal_log_table` (`log_id`);
 
 -- 
