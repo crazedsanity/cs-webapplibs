9a1ba0486c76d6d80178d2dc7a1142b52d7c98c9
crazedsanity <crazedsanity@59f08903-b221-44ba-a638-31359e0048a4>
2011-01-26 04:25:41 +0000

    Drop Generic Data Linker code, since it was flawed and really isn't used.

--- trunk/0.4/setup/schema.mysql.sql
+++ trunk/0.4/setup/schema.mysql.sql
@@ -168,33 +168,6 @@ CREATE TABLE `cswal_version_table` (
 
 
 -- 
--- Table for cs_genericDataLinker{}.
--- 
-CREATE TABLE `cswal_gdl_object_table` (
-	`object_id` int(11) NOT NULL PRIMARY KEY auto_increment,
-	`object_name` text NOT NULL UNIQUE
-) ENGINE=InnoDB;
-
-
-CREATE TABLE `cswal_gdl_path_table` (
-	`path_id` int(11) NOT NULL PRIMARY KEY auto_increment,
-	`path_id_list` text NOT NULL UNIQUE
-) ENGINE=InnoDB;
-
--- 
--- Store attributes here.
--- NOTE: fields prefixed with "a_" to avoid clashes with reserved words.
--- 
-CREATE TABLE `cswal_gdl_attribute_table` (
-	`attribute_id` serial NOT NULL PRIMARY KEY auto_increment,
-	`object_path` text NOT NULL,
-	`a_text` text DEFAULT NULL,
-	`a_int` integer DEFAULT NULL,
-	`a_dec` decimal(10,2) DEFAULT NULL,
-	`a_bool` boolean DEFAULT NULL
-) ENGINE=InnoDB;
-
--- 
 -- Constraints for dumped tables
 -- 
 
