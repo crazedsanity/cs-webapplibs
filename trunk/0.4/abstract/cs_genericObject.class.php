adb05c45bc28f13594e8fd41322790cff97da474
crazedsanity <crazedsanity@59f08903-b221-44ba-a638-31359e0048a4>
2011-01-27 14:53:53 +0000

    Class for handling generic objects (for the permissions system, but could be re-used).

---
+++ trunk/0.4/abstract/cs_genericObject.class.php
@@ -0,0 +1,92 @@
+<?php
+
+/*
+ * FILE INFORMATION:
+ * 
+ * $HeadURL$
+ * $Id$
+ * $LastChangedDate$
+ * $LastChangedBy$
+ * $LastChangedRevision$
+ */
+
+abstract class cs_genericObject extends cs_genericUserGroupAbstract {
+	
+	/** Table name used to store object records. */
+	protected $oTable = "cswal_object_table";
+	
+	/** Sequence for object table. */
+	protected $oSeq = "cswal_object_table_object_id_seq";
+	
+	/** dbTableHandler{} object for simplifying SQL. */
+	private $dbTableHandler;
+	
+	//============================================================================
+	public function __construct(cs_phpDB $db) {
+		parent::__construct($db);
+		$cleanString = array(
+			'object_name'	=> 'text'
+		);
+		$this->dbTableHandler = new cs_dbTableHandler($this->db, $this->oTable, $this->oSeq, 'group_id', $cleanString);
+	}//end __construct()
+	//============================================================================
+	
+	
+	
+	//============================================================================
+	public function create_object($objectName) {
+		if(strlen($objectName)) {
+			$newId = $this->dbTableHandler->create_record(array('object_name', $objectName));
+		}
+		else {
+			throw new exception(__METHOD__ .": invalid object name (". $objectName .")");
+		}
+		return($newId);
+	}//end create_object()
+	//============================================================================
+	
+	
+	
+	//============================================================================
+	public function get_object_by_name($objectName) {
+		if(strlen($objectName)) {
+			try {
+				$retval = $this->dbTableHandler->get_single_record(array('object_name'=>$objectName));
+			}
+			catch(Exception $e) {
+				throw new exception(__METHOD__ .":: failed to object from name=(". $objectName ."), DETAILS::: ". $e->getMessage());
+			}
+		}
+		else {
+			throw new exception(__METHOD__ .":: invalid object name (". $objectName .")");
+		}
+		return($retval);
+	}//end get_object_by_name()
+	//============================================================================
+	
+	
+	
+	//============================================================================
+	public function get_object_by_id($objectId) {
+		if(strlen($objectName)) {
+			try {
+				$retval = $this->dbTableHandler->get_record_by_id($objectId);
+			}
+			catch(Exception $e) {
+				throw new exception(__METHOD__ .":: failed to object for ID=(". $objectId ."), DETAILS::: ". $e->getMessage());
+			}
+		}
+		else {
+			throw new exception(__METHOD__ .":: invalid object ID (". $objectId .")");
+		}
+		return($retval);
+	}//end get_object_by_id()
+	//============================================================================
+	
+	
+	
+	//============================================================================
+	//============================================================================
+	
+}
+?>
