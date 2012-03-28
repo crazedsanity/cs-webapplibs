941e873b3118271d69bc107e4380a83e34b1c622
Dan Falconer <github@crazedsanity.com>
2012-03-27 20:05:06 -0500

    Code from CS, for registration.

---
+++ authUser.class.php
@@ -0,0 +1,302 @@
+<?php
+
+
+/* SVN INFORMATION::::
+ * --------------------------
+ * $HeadURL: https://svn.crazedsanity.com/svn/main/sites/crazedsanity.com/trunk/lib/authUser.class.php $
+ * $Id: authUser.class.php 1908 2011-07-20 00:50:31Z danf $
+ * $LastChangedDate: 2011-07-19 19:50:31 -0500 (Tue, 19 Jul 2011) $
+ * $LastChangedRevision: 1908 $
+ * $LastChangedBy: danf $
+ */
+
+require_once(dirname(__FILE__) .'/cs-content/cs_session.class.php');
+require_once(dirname(__FILE__) .'/cs-webapplibs/cs_phpDB.class.php');
+require_once(dirname(__FILE__) .'/cs-content/cs_globalFunctions.class.php');
+
+class authUser extends cs_session {
+	
+	/** Database connection object */
+	protected $dbObj;
+	
+	/** cs_globalFunctions class. */
+	protected $gfObj;
+	
+	/** Information about the logged-in user. */
+	protected $userInfo=array();
+	
+	/** Name of cookie that will be set... */
+	const COOKIE_NAME='CS_SESSID';
+	
+	/** Cached data from check_sid() */
+	protected $isAuthenticated=NULL;
+	
+	//-------------------------------------------------------------------------
+	public function __construct() {
+
+		//make sure the session has been created.
+		parent::__construct(self::COOKIE_NAME);
+		
+		$parameters = array(
+			'host'		=> $GLOBALS['DB_PG_HOST'],
+			'dbname'	=> $GLOBALS['DB_PG_DBNAME'],
+			'port'		=> $GLOBALS['DB_PG_PORT'],
+			'user'		=> $GLOBALS['DB_PG_DBUSER'],
+			'password'	=> $GLOBALS['DB_PG_DBPASS'],
+		);
+		
+		$this->dbObj = new cs_phpDB('pgsql');
+		$this->dbObj->connect($parameters);
+		
+		$this->gfObj = new cs_globalFunctions;
+		$this->logger = new cs_webdblogger($this->dbObj, "Auth", false);
+		if($this->is_authenticated()) {
+			$this->userInfo = $_SESSION['auth']['userInfo'];
+			if(!isset($_SESSION['uid']) || $_SESSION['uid'] != $_SESSION['auth']['userInfo']['uid']) {
+				$_SESSION['uid'] = $_SESSION['auth']['userInfo']['uid'];
+			}
+		}
+		else {
+			unset($_SESSION['auth']['userInfo']);
+		}
+	}//end __construct()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	protected function do_log($details, $type=NULL) {
+		if(is_null($type) || strlen($type) < 3) {
+			$type = "info";
+		}
+		try {
+			$this->logger->log_by_class($details, $type);
+		}
+		catch(Exception $ex) {
+			throw new exception(__METHOD__ .": failed to create log::: ". $ex->getMessage());
+		}
+	}//end do_log()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	public function is_authenticated() {
+		return($this->check_sid());
+	}//end is_authenticated()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	public function check_sid() {
+		//check the database to see if the sid is valid.
+		$sql = "SELECT * FROM cs_session_table WHERE session_id='". $this->sid ."'";
+		$numrows = $this->run_sql($sql);
+		
+		$retval = false;
+		if($numrows == 1) {
+			$retval = true;
+		}
+		$this->isAuthenticated = $retval;
+		
+		if($retval && !strlen($_SESSION['auth']['userInfo']['username'])) {
+			//something broke, and username isn't set.
+			if(is_numeric($_SESSION['auto']['userInfo']['uid']) && $_SESSION['auto']['userInfo']['uid'] > 0) {
+				$this->do_log("Username not set, pulling from the database...", 'error');
+				$this->update_auth_data($this->get_user_data($_SESSION['auto']['userInfo']['uid']));
+			}
+			else {
+				//something went wrong, log 'em out.
+				$this->do_log("Username not set, couldn't find UID in session, so logging them out...", 'error');
+				$this->logout_sid();
+				$retval = false;
+				$this->isAuthenticated = $retval;
+			}
+		}
+		#if($retval) {
+			$this->do_cookie();
+		#}
+		
+		return($retval);
+	}//end check_sid()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	public function login($username, $password) {
+		if($this->is_authenticated()) {
+			$this->do_log("User (". $username .") is already logged in, can't login again", 'error');
+			$retval = false;
+		}
+		else {
+			$sql = "SELECT * FROM cs_authentication_table WHERE username='". $username ."' " .
+				"AND passwd='". md5($username .'-'. $password) ."' AND user_status_id=1";
+			
+			$numrows = $this->run_sql($sql);
+			$retval = $numrows;
+			
+			if($numrows == 1) {
+				$data = $this->dbObj->farray_fieldnames();
+				$this->userInfo = $data;
+				$this->update_auth_data($this->userInfo);
+				$insertData = array(
+					'create_date'	=> "NOW()",
+					'session_id'	=> $this->sid,
+					'uid'			=> $data['uid'],
+					'ip'			=> $_SERVER['REMOTE_ADDR']
+				);
+				
+				$sql = 'INSERT INTO cs_session_table '. $this->gfObj->string_from_array($insertData, 'insert', NULL, 'sql');
+				$retval = $this->run_sql($sql);
+				
+				$this->run_sql("UPDATE cs_authentication_table SET last_login='NOW()' WHERE uid=". $data['uid']);
+				$this->do_log("Successfully logged-in (". $retval .")");
+			}
+			
+			if($retval == 1) {
+				$this->do_cookie();
+			}
+			$retval = $this->check_sid();
+			$this->do_log("Return value from check_sid() was (". $retval .")", 'debug');
+		}
+		
+		return($retval);
+	}//end login()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	protected function do_cookie() {
+		$cookieExpiration = '120 days';
+		if(defined('SESSION_MAX_TIME')) {
+			$cookieExpiration = constant('SESSION_MAX_TIME');
+		}
+		$createCookieRes = $this->create_cookie(self::COOKIE_NAME, $this->sid, $cookieExpiration, '/', '.crazedsanity.com');
+		#$this->do_log("Result of creating cookie: ". $createCookieRes, 'debug');
+		return($createCookieRes);
+	}//end do_cookie()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	protected function get_user_data($uid) {
+		if(is_numeric($uid) && $uid > 0) {
+			$sql = "SELECT * FROM cs_authentication_table WHERE uid=". $uid 
+					." AND user_status_id=1";
+			$numrows = $this->run_sql($sql);
+			
+			if($numrows == 1) {
+				$retval = $this->dbObj->farray_fieldnames();
+			}
+			else {
+				//
+				$details = __METHOD__ .": failed to retrieve a single user (". $numrows .") for uid=(". $uid .")";
+				$this->do_log($details, 'exception in code');
+				throw new exception($details);
+			}
+		}
+		else {
+			$details = __METHOD__ .": invalid uid (". $uid .")";
+			$this->do_log($details, 'exception in code');
+			throw new exception($details);
+		}
+		
+		return($retval);
+	}//end get_user_data()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	protected function update_auth_data(array $data) {
+		$_SESSION['auth']['userInfo'] = $data;
+	}//end update_auth_data()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	public function logout_sid() {
+		$retval = false;
+		if($this->isAuthenticated) {
+			$sql = "DELETE FROM cs_session_table WHERE session_id='". $this->sid ."'";
+			$retval = $this->run_sql($sql);
+			$dropCookieRes = $this->drop_cookie(self::COOKIE_NAME);
+			$this->do_log("Logged-out user with result (". $retval ."), removed cookie (". self::COOKIE_NAME .") with result (". $dropCookieRes .")", 'debug');
+		}
+		$this->check_sid(FALSE);
+		
+		return($retval);
+	}//end logout_sid()
+	//-------------------------------------------------------------------------
+	
+	
+	//-------------------------------------------------------------------------
+	private function run_sql($sql) {
+		$numrows = $this->dbObj->exec($sql);
+		$dberror = $this->dbObj->errorMsg();
+		
+		if(strlen($dberror) || !is_numeric($numrows) || $numrows < 0) {
+			throw new exception(__METHOD__ .": invalid numrows (". $numrows .") or database error: ". $dberror ."<BR>\nSQL: ". $sql);
+		}
+		else {
+			$retval = $numrows;
+		}
+		
+		return($retval);
+	}//end run_sql()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	public function checkin() {
+		$retval = NULL;
+		if($this->is_authenticated()) {
+			$sql = "UPDATE cs_session_table SET last_checkin='NOW()', " .
+				"num_checkins=num_checkins+1 WHERE session_id='". $this->sid ."';";
+			$retval = $this->run_sql($sql);
+		}
+		$this->logout_inactive_sessions();
+		
+		return($retval);
+	}//end checkin()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	public function get_user_info($index) {
+		$retval = NULL;
+		if(isset($this->userInfo[$index])) {
+			$retval = $this->userInfo[$index];
+		}
+		return($retval);
+	}//end get_user_info()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	private function logout_inactive_sessions() {
+		$maxIdle = '3 days';
+		if(defined('SESSION_MAX_IDLE')) {
+			$maxIdle = constant('SESSION_MAX_IDLE');
+		}
+		$sql = "DELETE FROM cs_session_table WHERE last_checkin  < (NOW() - interval '". $maxIdle ."')";
+		$numrows = $this->run_sql($sql);
+		
+		if($numrows < 0 || !is_numeric($numrows)) {
+			$details = __METHOD__ .": invalid numrows (". $numrows .")";
+			$this->do_log($details, 'exception in code');
+			throw new exception($details);
+		}
+		
+		return($numrows);
+	}//end logout_inactive_sessions()
+	//-------------------------------------------------------------------------
+}//end authUser{}
---
+++ registerUser.class.php
@@ -0,0 +1,333 @@
+<?php
+
+
+/* SVN INFORMATION::::
+ * --------------------------
+ * $HeadURL: https://svn.crazedsanity.com/svn/main/sites/crazedsanity.com/trunk/lib/registerUser.class.php $
+ * $Id: registerUser.class.php 1909 2011-07-20 00:50:57Z danf $
+ * $LastChangedDate: 2011-07-19 19:50:57 -0500 (Tue, 19 Jul 2011) $
+ * $LastChangedRevision: 1909 $
+ * $LastChangedBy: danf $
+ */
+
+class registerUser {
+	
+	/** Database connection object */
+	private $dbObj;
+	
+	/** cs_globalFunctions class. */
+	private $gfObj;
+	
+	public $debug=array();
+	
+	//-------------------------------------------------------------------------
+	public function __construct() {
+		
+		$parameters = array(
+			'host'		=> $GLOBALS['DB_PG_HOST'],
+			'dbname'	=> $GLOBALS['DB_PG_DBNAME'],
+			'port'		=> $GLOBALS['DB_PG_PORT'],
+			'user'		=> $GLOBALS['DB_PG_DBUSER'],
+			'password'	=> $GLOBALS['DB_PG_DBPASS'],
+		);
+		
+		$this->dbObj = new cs_phpDB('pgsql');
+		$this->dbObj->connect($parameters);
+		
+		$this->logger = new cs_webdblogger($this->dbObj, 'Registration');
+		
+		$this->gfObj = new cs_globalFunctions;
+	}//end __construct()
+	//-------------------------------------------------------------------------
+	
+	
+	//-------------------------------------------------------------------------
+	private function run_sql($sql) {
+		$numrows = $this->dbObj->exec($sql);
+		$dberror = $this->dbObj->errorMsg();
+		
+		if(strlen($dberror) || !is_numeric($numrows) || $numrows < 0) {
+			$details = __METHOD__ .": invalid numrows (". $numrows .") or database error: ". $dberror ."<BR>\nSQL: ". $sql;
+			$this->logger->log_by_class($details, 'code exception');
+			throw new exception($details);
+		}
+		else {
+			$retval = $numrows;
+		}
+		
+		return($retval);
+	}//end run_sql()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	public function check_username_available($username) {
+		$retval = false;
+		$sql = "SELECT * FROM cs_authentication_table WHERE username='".
+			$this->gfObj->cleanString($username, 'sql') ."'";
+		
+		$check = $this->run_sql($sql);
+		if($check == 0) {
+			$retval = true;
+		}
+		$this->logger->log_by_class(__METHOD__ .": username=[". $username ."], result=(". 
+				$this->gfObj->interpret_bool($retval, array(0,1)) .")", 'precheck');
+		return($retval);
+	}//end check_username_available()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	public function check_password_complexity($password, $passwordCheck) {
+		$retval = array(
+			'result'	=> false,
+			'passcheck'	=> 0,
+			'info'		=> "Password too short or not given"
+		);
+		if($password != $passwordCheck) {
+			$retval['info'] = "Passwords don't match.";
+		}
+		elseif(!is_null($password) && strlen($password) <8) {
+			$retval['info'] = "Password too short; must be at least 8 characters.";
+		}
+		elseif(!is_null($password)) {
+			if($password == $passwordCheck) {
+				$retval['passcheck'] = 1;
+				$regexList = array(
+					'one number'			=> '/[0-9]{1,}/',
+					'one lowercase letter'	=> '/[a-z]{1,}/',
+					'one uppercase letter'	=> '/[A-Z]{1,}/',
+				);
+				$passes = 0;
+				$retval['info'] = "";
+				foreach($regexList as $text=>$regex) {
+					$passFailText="FAIL";
+					if(preg_match($regex, $password)) {
+						$passes++;
+						$passFailText = "ok";
+					}
+					else {
+						$retval['info'] = $this->gfObj->create_list($retval['info'], $text, " and ");
+					}
+				}
+				if($passes == count($regexList)) {
+					$retval['result'] = true;
+				}
+				else {
+					$retval['info'] = "Password must contain at least one ". $retval['info'];
+				}
+			}
+			else {
+				$retval['info'] = "passwords don't match";
+			}
+		}
+		$this->logger->log_by_class(__METHOD__ .": result=(". 
+				$this->gfObj->interpret_bool($retval['result'], array(0,1)) ."), "
+						."passcheck=(". $retval['passcheck'] .")", 'precheck');
+		
+		return($retval);
+	}//end check_password_complexity()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	public function check_email_validity($emailAddr, $giveReasonOnFail=false) {
+		$retval = false;
+		if(strlen($emailAddr)) {
+			$cleaned = $this->gfObj->cleanString($emailAddr, 'email');
+			//the length assumes an email with the smallest form being 'jd@xy.com'
+			$emailRegex = '/^[A-Z0-9\._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i';
+			if($emailAddr == $cleaned && preg_match($emailRegex, $emailAddr)) {
+				$retval = true;
+			}
+			
+			if($giveReasonOnFail == true) {
+				$debug = "";
+				if($emailAddr != $cleaned) {
+					$debug .= "cleaned does NOT match original";
+				}
+				if(!preg_match($emailRegex, $emailAddr)) {
+					$debug .= " || regex failed";
+				}
+				$retval = "provided email=(". $emailAddr ."), cleaned email=(". $cleaned ."), retval=(". $retval ."), debug=(". $debug .")";
+			}
+		}
+		else {
+			$details = __METHOD__ .": no valid data provided";
+			$this->logger->log_by_class($details, 'code exception');
+			throw new exception($details);
+		}
+		
+		$this->logger->log_by_class(__METHOD__ .": email=(". $emailAddr ."), retval=(". $retval .")", 'precheck');
+		
+		return($retval);
+	}//end check_email_validity()
+	//-------------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------------
+	public function register_user($username, $password, $checkPassword, $email) {
+		if(strlen($username) && strlen($password) && strlen($checkPassword) && strlen($email)) {
+			if($this->check_username_available($username)) {
+				//check passwords.
+				$passCheck = $this->check_password_complexity($password, $checkPassword);
+				if($passCheck['result'] === true) {
+					if($this->check_email_validity($email)) {
+						//this is where we attempt to insert the user's information.
+						$insertData = array(
+							'username'	=> $username,
+							'passwd'	=> md5($username .'-'. $password),
+							'email'		=> $email
+						);
+						$insertSql = "INSERT INTO cs_authentication_table " 
+								. $this->gfObj->string_from_array($insertData, 'insert');
+						
+						if($this->run_sql($insertSql) == 1) {
+							$this->run_sql("SELECT currval('cs_authentication_table_uid_seq')");
+							$data = $this->dbObj->farray();
+							$retval = $data[0];
+							
+							//now let's build the activation email.
+							require_once(constant('LIBDIR') ."/phpmailer/class.phpmailer.php");
+							require_once(constant('LIBDIR') ."/cs-content/cs_genericPage.class.php");
+							$activateData = array(
+								'email'			=> $email,
+								'username'		=> $username,
+								'activateHash'	=> md5($retval .'-'. $username),
+								'uid'			=> $retval,
+								'HOST'			=> $GLOBALS['pageObj']->templateVars['HOST'],
+								'username'		=> $username,
+								'password'		=> $password
+							);
+							
+							$this->logger->log_by_class("Created uid (". $retval ."), username=(". $username ."), "
+									."email=(". $email .")", 'create');
+							
+							$this->debug[__METHOD__] = $this->send_activation_email($activateData);
+						}
+						else {
+							$details = __METHOD__ .": failed to create new user";
+							$this->logger->log_by_class($details, 'exception');
+							throw new exception($details);
+						}
+					}
+					else {
+						$details = __METHOD__ .": invalid email address (". $email .")";
+						$this->logger->log_by_class($details, 'exception');
+						throw new exception($details);
+					}
+				}
+				else {
+					$details = __METHOD__ .": password complexity failed";
+					$this->logger->log_by_class($details, 'exception');
+					throw new exception($details);
+				}
+			}
+			else {
+				$details = __METHOD__ .": username (". $username .") is not available";
+				$this->logger->log_by_class($details, 'exception');
+				throw new exception($details);
+			}
+		}
+		else {
+			$details = __METHOD__ .": one or more required fields missing";
+			$this->logger->log_by_class($details, 'exception');
+			throw new exception($details);
+		}
+		
+		return($retval);
+	}//end register_user()
+	//-------------------------------------------------------------------------
+	
+	
+	//-------------------------------------------------------------------
+	function send_activation_email($infoArr) {
+		//setup the variables for sending the email...
+		$to   = $infoArr['email'];
+		$subj = "New Account Activation [". $infoArr['uid'] ."]";
+		
+		//read-in the contents of the email template.
+		$page = new cs_genericPage(false, "content/registrationEmail.tmpl");
+		
+		//parse the template...
+		foreach($infoArr as $tmpl=>$value) {
+			$page->add_template_var($tmpl,$value);
+		}
+		#$body = mini_parser($tmpl, $infoArr, "{", "}");
+		$body = $page->return_printed_page();
+		
+		$result = $this->send_single_email($to, $subj, $body);
+		$this->debug[__METHOD__] = $result;
+		
+		$this->logger->log_by_class($this->gfObj->debug_print($this->debug,0), 'debug');
+		
+		return($result);
+	}//end send_activation_email()
+	//-------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------
+	/**
+	 * Send an email to a single address with no special parsing.
+	 */
+	function send_single_email($toAddr, $subject, $body) {
+			
+		$mail = new PHPMailer();
+		$mail->SetLanguage("en");
+		$mail->IsSendmail();
+		
+		$mail->IsMail();
+		//$mail->IsSMTP;
+		$mail->Host = "127.0.0.1";
+		$mail->From = "newAccounts@crazedsanity.com";
+		$mail->FromName = "New User Registration";
+		$mail->AddAddress($toAddr);
+		$mail->ContentType = "text/plain";
+		$mail->Subject = $subject;
+		$mail->Body = $body;
+		
+		$result = $mail->Send();
+		$this->debug[__METHOD__] = $result;
+		return($toAddr);
+	}//end send_single_email()
+	//-------------------------------------------------------------------
+	
+	
+	
+	//-------------------------------------------------------------------
+	public function activate_user($uid, $hash) {
+		$activateRes = false;
+		if(is_numeric($uid) && strlen($hash) == 32) {
+			$sql = "SELECT * FROM cs_authentication_table WHERE uid=". $uid;
+			try {
+				$data = $this->dbObj->run_query($sql);
+				
+				$checkHash = md5($uid .'-'. $data['username']);
+				if($hash == $checkHash) {
+					$sql = "UPDATE cs_authentication_table SET user_status_id =1 WHERE uid=". $uid;
+					if($this->dbObj->run_update($sql									)) {
+						//TODO: log it.
+						$activateRes = true;
+					}
+				}
+				else {
+					//TODO: log something here.
+				}
+			}
+			catch(exception $e) {
+				throw new exception(__METHOD__ .": running SQL failed::: ". $sql);
+			}
+		}
+		return($activateRes);
+	}//end activate_user()
+	//-------------------------------------------------------------------
+	
+	
+	
+	
+}//end authUser{}
