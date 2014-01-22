a7a2858249761633f43da75ac92693625d889674
Dan Falconer <github@crazedsanity.com>
2014-01-22 10:46:35 -0600

    More tests

--- tests/WebDBUpgradeTest.php
+++ tests/WebDBUpgradeTest.php
@@ -2,12 +2,19 @@
 
 class testOfCSWebDbUpgrade extends testDbAbstract {
 	
+	public $fileToVersion = array();
 	
 	//--------------------------------------------------------------------------
 	function __construct() {
 		$this->gfObj = new cs_globalFunctions;
 		$this->gfObj->debugPrintOpt=1;
 		parent::__construct();
+		
+		$this->fileToVersion = array(
+			dirname(__FILE__). '/files/VERSION-1'	=> "1.0.0-RC8000312",
+			dirname(__FILE__) .'/files/VERSION-2'	=> "1.9.0",
+			dirname(__FILE__) .'/files/VERSION-3'	=> "8.0.4003-RC2"
+		);
 	}//end __construct()
 	//--------------------------------------------------------------------------
 	
@@ -18,7 +25,7 @@ class testOfCSWebDbUpgrade extends testDbAbstract {
 		$this->gfObj = new cs_globalFunctions;
 		$this->gfObj->debugPrintOpt=1;
 		
-		$this->reset_db();
+		$this->reset_db(dirname(__FILE__) .'/../setup/schema.pgsql.sql');
 		parent::setUp();
 	}//end setUp()
 	//--------------------------------------------------------------------------
@@ -34,7 +41,7 @@ class testOfCSWebDbUpgrade extends testDbAbstract {
 	
 	
 	//--------------------------------------------------------------------------
-	public function test_basic_functions() {
+	public function test_versionParsing() {
 		$upgObj = new upgradeTester();
 		
 		// These should all pass muster.
@@ -84,31 +91,242 @@ class testOfCSWebDbUpgrade extends testDbAbstract {
 			$this->assertTrue(is_numeric($vArr['number']), "Number (". $vArr['number'] .") isn't a number...?");
 		}
 		
-	}//end test_basic_functions()
+	}
 	//--------------------------------------------------------------------------
 	
 	
 	
 	//--------------------------------------------------------------------------
-	public function test_stuff() {
+	public function test_dbVersions() {
+		$x = new upgradeTester();
+		$x->db = $this->dbObj;
+		$x->versionFileLocation = dirname(__FILE__) .'/files/VERSION-1';
+		$x->upgradeConfigFile = dirname(__FILE__) .'/files/upgrade.ini';
+		
+		$this->assertEquals('1.0.0-RC8000312', $x->read_version_file());
+		$this->assertEquals('cs-webapplibstest', $x->projectName);
+		
+		
+		$testThis = $x->get_database_version();
+		$this->assertEquals("", $testThis);
+		$setRes = $x->set_initial_version('0.2.1-ALPHA3');
+		$this->assertTrue($setRes);
+		
+		$dbVersion = $x->get_database_version();
+		$this->assertEquals('0.2.1-ALPHA3', $dbVersion['version_string']);
+		$this->assertEquals('0', $dbVersion['version_major']);
+		$this->assertEquals('2', $dbVersion['version_minor']);
+		$this->assertEquals('1', $dbVersion['version_maintenance']);
+		$this->assertEquals('ALPHA3', $dbVersion['version_suffix']);
+		
+		$updateRes = $x->update_database_version('1.2.3-RC1');
+		$this->assertTrue((bool)$updateRes);
+		$this->assertTrue($x->check_database_version($x->parse_version_string('1.2.3-RC1')));
+		$this->assertEquals($x->parse_version_string('1.2.3-RC1'), $x->get_database_version());
+	}
+	//--------------------------------------------------------------------------
+	
+	
+	
+	//--------------------------------------------------------------------------
+	public function test_lockingLogic() {
+		$x = new upgradeTester();
+		$x->lockObj = new cs_lockfile("unittest.lock");
+		$x->databaseVersion = '1.2.3';
+		
+		try {
+			$this->assertFalse($x->is_upgrade_in_progress());
+			$this->assertTrue($x->set_upgrade_in_progress());
+			$this->assertTrue($x->is_upgrade_in_progress());
+			$this->assertTrue($x->lockObj->delete_lockfile());
+			$this->assertFalse($x->is_upgrade_in_progress());
+		}
+		catch(Exception $ex) {
+			$this->fail(__METHOD__ .": unable to create lock... ". $ex->getMessage());
+		}
+	}
+	//--------------------------------------------------------------------------
+	
+	
+	
+	//--------------------------------------------------------------------------
+	public function test_exceptionNoVersionData() {
+		$x = new upgradeTester();
+		$x->db = $this->dbObj;
+		$x->projectName = __FUNCTION__;
+		
+		try {
+			$x->allowNoDBVersion = true;
+			$this->assertFalse($x->get_database_version());
+		}
+		catch(Exception $ex) {
+			$this->fail("unexpected exception after allowing no version: ". $ex->getMessage());
+		}
+		
+		try {
+			$x->allowNoDBVersion = null;
+			$x->get_database_version();
+			$this->fail("Database version is empty, it should have succeeded");
+		}
+		catch(Exception $ex2) {
+		}
+	}
+	//--------------------------------------------------------------------------
+	
+	
+	
+	//--------------------------------------------------------------------------
+	public function test_versionConflict() {
+		$x = new upgradeTester();
+		$x->db = $this->dbObj;
+		
+		$initialVersion = '1.2.3';
+		$x->versionFileVersion = $initialVersion;
+		$x->projectName = __FUNCTION__;
+		$x->set_initial_version('1.2.3');
+		
+		$this->assertFalse($x->check_for_version_conflict());
+		$x->versionFileVersion = '1.2.4';
+		$this->assertEquals('maintenance', $x->check_for_version_conflict());
+		
+		$x->versionFileVersion = '1.3.0';
+		$this->assertEquals('minor', $x->check_for_version_conflict());
+		
+		$x->versionFileVersion = '2.0.0';
+		$this->assertEquals('major', $x->check_for_version_conflict());
+		
+		$x->versionFileVersion = '1.2.3-ALPHA1';
+		$this->assertEquals('suffix', $x->check_for_version_conflict());
+		
+		$x->versionFileVerison = '1.2.3-BETA1';
+		$this->assertEquals('suffix', $x->check_for_version_conflict());
+		
+		$x->versionFileVersion = '1.2.3-RC1';
+		$this->assertEquals('suffix', $x->check_for_version_conflict());
+	}
+	//--------------------------------------------------------------------------
+	
+	
+	//TODO: test exceptions about unsupported downgrades...
+	
+	
+	
+	
+	//--------------------------------------------------------------------------
+	public function test_versionFile() {
+		$x = new upgradeTester;
+		$x->db = $this->dbObj;
+		
+		foreach($this->fileToVersion as $file => $version) {
+			$this->assertTrue(file_exists($file));
+			
+			$x->versionFileLocation = $file;
+			$this->assertEquals($version, $x->read_version_file());
+		}
+	}//end test_versionFile()
+	//--------------------------------------------------------------------------
+	
+	
+	
+	//--------------------------------------------------------------------------
+	public function test_reconnectDb() {
+		$x = new upgradeTester;
+		$x->db = $this->dbObj;
+		$x->projectName = __FUNCTION__;
+		
+		//
+		$this->assertFalse($x->db->get_transaction_status());
+		$this->assertTrue($x->set_initial_version('1.0.0'));
+		$this->assertEquals($x->parse_version_string('1.0.0'), $x->get_database_version());
+		
+		$this->assertTrue($x->db->beginTrans());
+		$this->assertTrue($x->db->get_transaction_status());
+		$this->assertEquals($x->parse_version_string('1.0.0'), $x->get_database_version());
+		
+		$this->assertTrue($x->db->get_transaction_status());
+		$this->assertTrue((bool)$x->update_database_version('2.0.0'));
+		$this->assertEquals($x->parse_version_string('2.0.0'), $x->get_database_version());
+		
+		//now reconnect, and test that the database version is NOT updated.
+		$this->assertTrue($x->reconnect_db());
+		$this->assertEquals($x->parse_version_string('1.0.0'), $x->get_database_version());
+		
+	}
+	//--------------------------------------------------------------------------
+	
+	
+	
+	//--------------------------------------------------------------------------
+	public function test_cache() {
+		$x = new upgradeTester();
+		$x->db = $this->dbObj;
+		$x->projectName = __FUNCTION__;
+		
+		$this->assertEquals(array(), $x->_getCache());
+		
+		$x->_setCache('test', array(1,2,3));
+		$this->assertTrue(is_array($x->_getCache()));
+		
+		$testThis = array(1,2,3);
+		$this->assertEquals($testThis, $x->_getCache('test'));
+		
+		$x->_setCache(null, null);
+		$this->assertEquals(array(), $x->_getCache());
+	}
+	//--------------------------------------------------------------------------
+	
+	
+	
+	//--------------------------------------------------------------------------
+	public function test_calls() {
+		$x = new upgradeTester();
+		
+		$this->assertEquals(0, $x->_getCalls());
+		$x->_setCalls(9999);
+		$this->assertEquals(9999, $x->_getCalls());
+	}
+	//--------------------------------------------------------------------------
+	
+	
+	
+	//--------------------------------------------------------------------------
+	public function xtest_normalCall() {
+		
+		/*
+		 * NOTE::: this is a *simulated* normal call, as using cs_webdbupgrade{} 
+		 *	directly would not allow testing of protected members.
+		 */
+		$x = new upgradeTester();
+		$x->projectName = __FUNCTION__;
+//		$x->set_initial_version('1.0.0');
+		$x->doSetup(
+				dirname(__FILE__) .'/files/VERSION-1', 
+				dirname(__FILE__) .'/files/upgrade.ini',
+				$this->dbObj
+		);
+		
+		$this->assertEquals(1, $x->_getCalls());
+		$this->assertEquals(__FUNCTION__, $x->projectName);
+		$this->assertEquals($x->parse_version_string('1.0.0'), $x->get_database_version());
+		$this->assertEquals(1, $x->_getCalls());
+	}
+	
+	
+	
+	//--------------------------------------------------------------------------
+	public function xtest_stuff() {
 		$upgObj = new upgradeTester();
 		$upgObj->gfObj = new cs_globalFunctions();
 		$upgObj->fsObj = new cs_fileSystem(dirname(__FILE__) .'/files');
 		
-		$fileToVersion = array(
-			dirname(__FILE__). '/files/VERSION-1'	=> "1.0.0-RC8000312",
-			dirname(__FILE__) .'/files/VERSION-2'	=> "1.9.0",
-			dirname(__FILE__) .'/files/VERSION-3'	=> "8.0.4003-RC2"
-		);
-		
-		foreach($fileToVersion as $file=>$version) {
+		foreach($this->fileToVersion as $file=>$version) {
 			$upgObj->versionFileLocation = $file;
 			if($this->assertTrue(file_exists($file))) {
 				try {
 					$this->assertEquals($version, $upgObj->read_version_file());
 				}
 				catch(Exception $e) {
-					//yeah...
+					$this->fail("Error reading version file: ". $e->getMessage());
 				}
 			}
 		}
@@ -135,7 +353,7 @@ class testOfCSWebDbUpgrade extends testDbAbstract {
 		
 		
 		//$upgObj->doSetup();
-	}//end test_stuff()
+	}
 	//--------------------------------------------------------------------------
 	
 	
@@ -156,7 +374,7 @@ class testOfCSWebDbUpgrade extends testDbAbstract {
 
 		$versionFileLocation = dirname(__FILE__) . '/files/VERSION-1';
 		$upgObj->set_version_file_location($versionFileLocation);
-		$upgObj->doSetup($versionFileLocation, $upgradeConfigFile, $this->dbObj);
+		$upgObj->doSetup($versionFileLocation, null, $this->dbObj);
 		if (!$this->assertEquals($upgObj->versionFileVersion, '1.9.0')) {
 			cs_global::debug_print($upgObj, 1);
 		}
@@ -236,18 +454,13 @@ class upgradeTester extends cs_webdbupgrade {
 	}//end check_for_version_conflict()
 	
 	
-	public function get_database_version() {
-		return(parent::get_database_version());
-	}//end get_database_version()
-	
-	
 	public function update_database_version($newVersionString) {
 		return(parent::update_database_version($newVersionString));
 	}//end update_database_version()
 	
 	
-	public function check_database_version() {
-		return(parent::check_database_version());
+	public function check_database_version(array $checkThis) {
+		return(parent::check_database_version($checkThis));
 	}//end check_database_version()
 	
 	
@@ -298,6 +511,41 @@ class upgradeTester extends cs_webdbupgrade {
 	public function check_internal_upgrades() {
 		parent::check_internal_upgrades();
 		cs_webdbupgrade::$cache = array();
+	}
+	
+	public function set_initial_version($versionString) {
+		return parent::set_initial_version($versionString);
+	}
+	
+	public function reconnect_db() {
+		return parent::reconnect_db();
+	}
+	
+	public function _getCache($index=null) {
+		if(!is_null($index)) {
+			$retval = parent::$cache[$index];
+		}
+		else {
+			$retval = parent::$cache;
+		}
+		return $retval;
+	}
+	
+	public function _setCache($name, $val) {
+		if(is_null($name) && is_null($val)) {
+			parent::$cache = array();
+		}
+		else {
+			parent::$cache[$name] = $val;
+		}
+	}
+	
+	public function _getCalls() {
+		return parent::$calls;
+	}
+	
+	public function _setCalls($val) {
+		parent::$calls = $val;
 	}
 	
 
