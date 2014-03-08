bc26195e9cdb8ee17df2c6d0cec12d92ea54431d
Dan Falconer <github@crazedsanity.com>
2014-03-07 20:42:07 -0600

    Updated unit tests

--- tests/AuthTokenTest.php
+++ tests/AuthTokenTest.php
@@ -1,211 +1,293 @@
 <?php
+
 /*
  * Created on Jan 25, 2009
  */
 
 class testOfCSAuthToken extends testDbAbstract {
-	
-	//--------------------------------------------------------------------------
+
 	function __construct() {
 		$this->gfObj = new cs_globalFunctions;
-		$this->gfObj->debugPrintOpt=1;
+		$this->gfObj->debugPrintOpt = 1;
 		parent::__construct();
-	}//end __construct()
-	//--------------------------------------------------------------------------
+	}
 	
 	
 	
-	//--------------------------------------------------------------------------
 	function setUp() {
 		$this->gfObj = new cs_globalFunctions;
-		$this->gfObj->debugPrintOpt=1;
+		$this->gfObj->debugPrintOpt = 1;
 		parent::setUp();
-		$this->assertTrue($this->reset_db(dirname(__FILE__) .'/../setup/schema.pgsql.sql'), "Failed to reset database");
-	}//end setUp()
-	//--------------------------------------------------------------------------
+		$this->assertTrue($this->reset_db(dirname(__FILE__) . '/../setup/schema.pgsql.sql'), "Failed to reset database");
+	}
 	
 	
 	
-	//--------------------------------------------------------------------------
 	public function tearDown() {
 		parent::tearDown();
-	}//end tearDown()
-	//--------------------------------------------------------------------------
+	}
 	
 	
 	
-	//--------------------------------------------------------------------------
 	public function test_creation() {
 		$x = new cs_authToken($this->dbObj);
-//		$this->assertTrue(is_object($x));
-		
+
 		$name = '_ToKen';
 		$value = __METHOD__;
 		$pass = 'foo@bar.com';
-		
+
 		$this->assertEquals($name, $x->create_token($pass, $value, $name));
-		
-		
+
+
 		$res = $x->remove_expired_tokens($name);
-		
+
 		$this->assertEquals($res['type'], $x::EXPIRE_SINGLE);
 		$this->assertEquals($res['num'], 0);
-		
+
 		$expectedResult = array(
-			'result'		=> true,
-			'reason'		=> $x::RESULT_SUCCESS,
-			'stored_value'	=> $value,
+			'result' => true,
+			'reason' => $x::RESULT_SUCCESS,
+			'stored_value' => $value,
 		);
 		$actualResult = $x->authenticate_token($name, $pass);
-		
-		foreach($expectedResult as $k=>$v) {
+
+		foreach ($expectedResult as $k => $v) {
 			$this->assertTrue(isset($actualResult[$k]));
-			$this->assertEquals($v, $actualResult[$k], "Value mismatch for '". $k ."', ACTUAL::: ". cs_global::debug_print($actualResult,0));
+			$this->assertEquals($v, $actualResult[$k], "Value mismatch for '" . $k . "', ACTUAL::: " . cs_global::debug_print($actualResult, 0));
 		}
 	}
-	//--------------------------------------------------------------------------
 	
 	
 	
-	//--------------------------------------------------------------------------
-	function XOLD_test_token_basics() {
-		$dbObj = $this->dbObj;
+	public function test_single_use_with_second_auth_attempt() {
+		$x = new cs_authToken($this->dbObj);
+		$pass = 'test123';
 		
-		#$dbObj->beginTrans();
-		$tok = new cs_authToken($dbObj);
-//		if($this->assertTrue($this->reset_db(dirname(__FILE__) .'/../setup/schema.pgsql.sql'), "Failed to reset database")) {
-			#$tok->load_schema($type, $dbObj);
-			$tok = new cs_authToken($dbObj);
-	
-			//Generic test to ensure we get the appropriate data back.
-			{
-				$tokenData = $tok->create_token(1, 'test', 'abc123');
-				$this->do_tokenTest($tokenData, 1, 'test');
-	
-				$this->assertEqual($tok->authenticate_token($tokenData['id'], 'test', $tokenData['hash']), 1);
-				$this->assertFalse($tok->authenticate_token($tokenData['id'], 'testx', $tokenData['hash']));
-				$this->assertFalse($tok->authenticate_token($tokenData['id'], 'test', 'abcdefg'));
-				$this->assertFalse($tok->authenticate_token($tokenData['id'], 'test', '12345678901234567890123456789012'));
-				$this->assertFalse($tok->authenticate_token(99999, 'test', '12345678901234567890123456789012'));
-	
-				//check to make sure the data within this token shows only ONE attempt.
-				$checkData = $tok->tokenData($tokenData['id']);
-				$this->assertEqual($checkData['auth_token_id'], $tokenData['id']);
-				$this->assertEqual($checkData['total_uses'], 1);
-			}
-	
-			//create a token with only 1 available use and try to authenticate it twice.
-			{
-				//Generic test to ensure we get the appropriate data back.
-				$tokenData = $tok->create_token(1, 'test', 'abc123', null, 1);
-				$this->do_tokenTest($tokenData, 1, 'test');
-	
-				$this->assertEqual($tok->authenticate_token($tokenData['id'], 'test', $tokenData['hash']), 1);
-				$this->assertTrue(($tok->authenticate_token($tokenData['id'], 'test', $tokenData['hash']) === null), 
-						"Able to authenticate twice on a token with only 1 use");
-				$this->assertFalse($tok->tokenData($tokenData['id'], true));
-				$this->assertFalse($tok->tokenData($tokenData['id'], false));
-			}
-	
-	
-			//now create a token with a maximum lifetime (make sure we can call it a ton of times)
-			{
-				//Generic test to ensure we get the appropriate data back.
-				$tokenData = $tok->create_token(1, 'test', 'abc123', '2 years');
-				$this->do_tokenTest($tokenData, 1, 'test');
-	
-				$this->assertEqual($tok->authenticate_token($tokenData['id'], 'test', $tokenData['hash']), 1);
-				$checkAttempts = 100;
-				$successAttempts = 0;
-				for($i=0; $i < 100; $i++) {
-					$id = $tok->authenticate_token($tokenData['id'], 'test', $tokenData['hash']);
-					if($this->assertEqual($tok->authenticate_token($tokenData['id'], 'test', $tokenData['hash']), 1)) {
-						$successAttempts++;
-					}
-				}
-				$this->assertEqual($checkAttempts, $successAttempts);
-			}
-	
-			//try to create a token with max_uses of 0.
-			{
-				$tokenData = $tok->create_token(2, 'test', 'xxxxyyyyyxxxx', null, 0);
-				$this->do_tokenTest($tokenData, 2, 'test');
-				$checkData = $tok->tokenData($tokenData['id']);
-	
-				$this->assertTrue(is_array($checkData));
-				$this->assertEqual($tokenData['id'], $checkData['auth_token_id']);
-				$this->assertEqual($checkData['max_uses'], null);
-			}
-	
-			//try creating a token that is purposely expired, make sure it exists, then make sure authentication fails.
-			{
-				$tokenData = $tok->create_token(2, 'test', 'This is a big old TEST', '-3 days');
-				if($this->assertTrue(is_array($tokenData))) {
-					$this->do_tokenTest($tokenData, 2, 'test');
-					$this->assertFalse($tok->authenticate_token($tokenData['id'], 'test', $tokenData['hash']));
-				}
-			}
-	
-			//make sure we don't get the same hash when creating multiple tokens with the same data.
-			//NOTE: this pushes the number of tests up pretty high, but I think it is required to help ensure hash uniqueness.
-			{
-				$uid=rand(1,999999);
-				$checksum = 'multiple ToKEN check';
-				$hashThis = "Lorem ipsum dolor sit amet. ";
-	
-				$numTests = 30;
-				$numPass = 0;
-				$tokenList = array();
-				for($i=0;$i<$numTests;$i++) {
-					$tokenList[$i] = $tok->create_token($uid, $checksum, $hashThis);
-				}
-				$lastItem = ($numTests -1);
-				for($i=0;$i<$numTests;$i++) {
-					$checkHash = $tokenList[$i]['hash'];
-					$uniq=0;
-					foreach($tokenList as $k=>$a) {
-						//check against everything BUT itself.
-						if($i != $k && $this->assertNotEqual($checkHash, $a['hash'])) {
-							$uniq++;
-						}
-					}
-					$this->assertEqual($uniq, ($numTests -1));
-				}
-			}
-	
-			//make sure the hash string isn't guessable, even if they can access our super-secret encryption algorithm. ;)
-			{
-				$uid = rand(1,99999);
-				$checksum = "my birfday";
-				$hashThis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque ut.";
-	
-				$tokenData = $tok->create_token($uid, $checksum, $hashThis);
-				$this->do_tokenTest($tokenData, $uid, $checksum);
-	
-				$this->assertNotEqual($tokenData['hash'], $tok->doHash($tokenData['id'], $uid, $checksum, $hashThis), 
-						"hash is guessable");
-			}
-	
-			//test expiring tokens...
-			{
-				//create a token that is immediately expired.
-				$tokenData = $tok->create_token(22, 'token expiration test', 'Lorem ipsum dolor sit amet, consectetur.', '-5 days');
-				$this->do_tokenTest($tokenData, 22, 'token expiration test');
-	
-				$this->assertFalse(is_array($tok->tokenData($tokenData['id'], true)));
-				$this->assertTrue(is_array($tok->tokenData($tokenData['id'], false)));
-				$this->assertTrue(count($tok->tokenData($tokenData['id'],false)) == 9);
-	
-				//REMEMBER: we've created other tokens that will now expire...
-				$removedTokens = $tok->remove_expired_tokens();
-				$this->assertEqual(2, $removedTokens);
-			}
-//		}
+		$id = $x->create_token('test123', null, null, null, 1);
 		
-	}//end test_token_basics()
-	//--------------------------------------------------------------------------
+		$this->assertTrue(is_string($id));
+		$this->assertTrue(strlen($id) == 40);
+		
+		$return = $x->authenticate_token($id, $pass);
+		
+		$this->assertTrue(is_array($return));
+		$this->assertTrue($return['result']);
+		
+		
+		$secondReturn = $x->authenticate_token($id, $pass);
+		$this->assertTrue(is_array($secondReturn));
+		$this->assertEquals(false, $secondReturn['result']);
+		
+		$this->assertEquals($x::RESULT_FAIL, $secondReturn['reason']);
+	}
 	
 	
+	
+	public function test_single_use_with_retrieval() {
+		$x = new cs_authToken($this->dbObj);
+		
+		$id = $x->create_token('test123', null, null, null, 1);
+		$firstData = $x->get_token_data($id);
+		$this->assertEquals(0, $firstData['total_uses']);
+		$this->assertEquals(1, $firstData['max_uses']);
+		
+		$res = $x->authenticate_token($id, 'test123');
+		$this->assertEquals(true, $res['result']);
+		$this->assertEquals($x::RESULT_SUCCESS, $res['reason']);
+		
+		$this->assertEquals(false, $x->get_token_data($id));
+		
+		$secondRes = $x->authenticate_token($id, 'test123');
+		$this->assertEquals($x::RESULT_FAIL, $secondRes['reason']);
+		$this->assertEquals(false, $secondRes['result']);
+	}
+	
+	
+	
+	public function xtest_limited_uses() {
+		$x = new cs_authToken($this->dbObj);
+		
+		$pass = 'test1234';
+		$maxUses = 10;
+		
+		
+		$id = $x->create_token($pass, __METHOD__, null, null, $maxUses);
+		
+		for($i=1; $i <= $maxUses; $i++) {
+			$data = $x->get_token_data($id);
+			
+			$this->assertEquals(($i - 1), $data['total_uses'], "total uses was not updated, expected $i, found (". $data['total_uses'] .")");
+			$this->assertEquals($id, $data['auth_token_id']);
+			$this->assertEquals(null, $data['expiration']);
+			
+			
+			$res = $x->authenticate_token($id, $pass);
+			
+			$this->assertEquals(true, $res['result']);
+			$this->assertEquals($x::RESULT_SUCCESS, $res['reason']);
+			$this->assertEquals(__METHOD__, $res['stored_value']);
+			$this->assertEquals(true, $res['result']);
+		}
+		
+		$finalData = $x->get_token_data($id);
+		$this->assertFalse(is_array($finalData));
+		$this->assertEquals(false, $finalData);
+		
+		$badAuth = $x->authenticate_token($id, $pass);
+		
+		$this->assertEquals(false, $badAuth['result']);
+		$this->assertEquals($x::RESULT_FAIL, $badAuth['reason']);
+		$this->assertFalse(isset($badAuth['stored_value']));
+	}
+	
+	
+	public function test_expiration() {
+		$x = new cs_authToken($this->dbObj);
+		
+		$id = $x->create_token('test123', __METHOD__, null, '-1 day');
+		
+		$res = $x->remove_expired_tokens($id);
+		
+		$this->assertEquals(false, $x->get_token_data($id));
+		$this->assertEquals($x::EXPIRE_SINGLE, $res['type']);
+		$this->assertEquals(1, $res['num']);
+		
+		$xRes = $x->authenticate_token($id, 'test123');
+		
+		$this->assertEquals(false, $xRes['result']);
+		$this->assertEquals($x::RESULT_FAIL, $xRes['reason']);
+		$this->assertFalse(isset($xRes['stored_value']));
+	}
+	
+	
+	
+	public function test_pre_expired() {
+		$x = new cs_authToken($this->dbObj);
+		
+		$pass = 'test21123#@)$!';
+		$id = $x->create_token($pass, __METHOD__, null, '-1 second');
+		
+		$tokenData = $x->get_token_data($id);
+		$this->assertTrue(strlen($tokenData['expiration']) > 0, "invalid expiration (". $tokenData['expiration'] .")");
+		$this->assertTrue(is_array($tokenData));
+		
+		$res = $x->authenticate_token($id, $pass);
+		$this->assertEquals(true, is_array($res));
+		$this->assertEquals($x::RESULT_EXPIRED, $res['reason']);
+	}
+	
+	
+	
+	public function test_unlimited_uses() {
+		$x = new cs_authToken($this->dbObj);
+		
+		$pass = 'test123';
+		$id = $x->create_token($pass, __METHOD__, null, null, 0);
+		
+		$data = $x->get_token_data($id);
+		
+		$this->assertEquals(0, $data['max_uses']);
+		$this->assertEquals(null, $data['expiration']);
+		
+		for($i=1; $i <= 10; $i++) {
+			$auth = $x->authenticate_token($id, $pass);
+			
+			$this->assertEquals(true, $auth['result']);
+			$this->assertEquals(__METHOD__, $auth['stored_value']);
+			$this->assertEquals($x::RESULT_SUCCESS, $auth['reason']);
+			
+			$data = $x->get_token_data($id);
+			
+			$this->assertEquals($i, $data['total_uses']);
+			$this->assertEquals(0, $data['max_uses']);
+			$this->assertEquals(null, $data['expiration']);
+			
+		}
+	}
+	
+	
+	
+	public function test_destroy_token() {
+		$x = new cs_authToken($this->dbObj);
+		
+		$id = $x->create_token('test123');
+		
+		$data = $x->get_token_data($id);
+		
+		$this->assertEquals(1, $data['max_uses']);
+		$this->assertEquals(null, $data['expiration']);
+		
+		$this->assertEquals(1, $x->destroy_token($id));
+		
+		$this->assertEquals(false, $x->get_token_data($id));
+		
+		try {
+			$x->destroy_token($id);
+			$this->fail("successfully destroyed non-existent token");
+		} catch (Exception $ex) {
+			if(preg_match('/failed to destroy token/', $ex->getMessage())) {
+				$this->fail("Unexpected/malformed error message::: ". $ex->getMessage());
+			}
+		}
+	}
+	
+	
+	
+	public function test_arbitrary_name() {
+		$x = new cs_authToken($this->dbObj);
+		
+		$myHash = $x->generate_token_string();
+		
+		$pass = 'test123';
+		$id = __FUNCTION__ .'/'. $myHash;
+		
+		$this->assertEquals($id, $x->create_token($pass, null, $id));
+		
+		$res = $x->authenticate_token($id, $pass);
+		
+		$this->assertEquals(true, $res['result']);
+		$this->assertEquals($x::RESULT_SUCCESS, $res['reason']);
+		$this->assertEquals(null, $res['stored_value']);
+	}
+	
+	
+	
+	public function test_store_array() {
+		$x = new cs_authToken($this->dbObj);
+		
+		$pass = 'tasdlfkj1123;asdfa0u';
+		
+		$myData = array();
+		for($i=0; $i < 1000; $i++) {
+			$myData[$i] = $x->generate_token_string();
+		}
+		
+		$id = $x->create_token($pass, $myData);
+		
+		$data = $x->get_token_data($id);
+		$res = $x->authenticate_token($id, $pass);
+		
+		$this->assertEquals(true, $res['result']);
+		$this->assertEquals($x::RESULT_SUCCESS, $res['reason']);
+		$this->assertEquals($myData, $res['stored_value']);
+		
+		$this->assertEquals(serialize($myData), $data['stored_value']);
+	}
+	
+	
+	
+	public function test_duplicates() {
+		$x = new cs_authToken($this->dbObj);
+		
+		$id = $x->create_token('test1234');
+		
+		try {
+			$newId = $x->create_token('asdasd', null, $id);
+			$this->fail("created duplicate token... ($id == $newId)");
+		} catch (Exception $ex) {
+			if(preg_match('/attempt to create non-unique token ($id)/', $ex->getMessage())) {
+				$this->fail("unexpected/malformed error::: ". $ex->getMessage());
+			}
+		}
+	}
 }
-
-?>
